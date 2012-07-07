73dd89f280209f25ab6fd33188b6125bf56be225
Johan Halin <johan.halin@me.com>
2012-07-07 13:58:35 +0300

    Public methods are now private.

--- Classes/MainViewController.h
+++ Classes/MainViewController.h
@@ -13,19 +13,6 @@
 
 @interface MainViewController : UIViewController <FlipsideViewControllerDelegate>
 
-- (IBAction)showInfo;
-- (IBAction)channel1ButtonPressed:(id)sender;
-- (IBAction)channel2ButtonPressed:(id)sender;
-- (IBAction)channel3ButtonPressed:(id)sender;
-- (IBAction)channel4ButtonPressed:(id)sender;
-- (IBAction)stopButtonPressed:(id)sender;
-- (void)createStreamer;
-- (void)destroyStreamer;
-- (void)resetEverything;
-- (void)updateNowPlaying;
-- (void)synchronousLoadNowPlayingData;
-- (void)updateNowPlayingLabel:(NSString *)track;
 - (void)resetAnimation;
-- (void)playSavedChannel;
 
 @end
