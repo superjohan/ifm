a319ff1d50a321d12043a0d23cecc50f9b2e8017
Tijmen Brommet <tijmen@gmail.com>
2024-08-10 22:37:03 +0200

    Refactor away lastStation
    lastStation was introduced to remember the current station after the
    user pressed pause in the iOS UI, because `currentStation` was being
    set to nil. The refactoring in January removed that, so we can make
    this a little easier.

--- IFMPlayer.swift
+++ IFMPlayer.swift
@@ -22,7 +22,6 @@ class IFMPlayer : NSObject {
     private var nowPlayingTimer: Timer? = nil
     private var nowPlayingText: String? = nil
     private var currentStation: IFMStation? = nil
-    private var lastStation: IFMStation? = nil
     
     var stateObservable: AnyPublisher<IFMPlayerState, Never> {
         self.statePublisher.eraseToAnyPublisher()
@@ -61,7 +60,6 @@ class IFMPlayer : NSObject {
         
         self.player = player
         self.currentStation = station
-        self.lastStation = station
 
         updateNowPlaying()
         updateState(.waiting(nowPlaying: self.nowPlayingText, stationIndex: self.stations.uiIndex(for: station)))
@@ -122,9 +120,9 @@ class IFMPlayer : NSObject {
         let commandCenter = MPRemoteCommandCenter.shared()
 
         commandCenter.playCommand.addTarget { _ in
-            guard let lastStation = self.lastStation else { return .noActionableNowPlayingItem }
+            guard let currentStation = self.currentStation else { return .noActionableNowPlayingItem }
 
-            self.play(channelIndex: self.stations.uiIndex(for: lastStation))
+            self.play(channelIndex: self.stations.uiIndex(for: currentStation))
             
             return .success
         }
@@ -138,8 +136,8 @@ class IFMPlayer : NSObject {
         commandCenter.togglePlayPauseCommand.addTarget { _ in
             if (self.player?.rate ?? 0) > 0.0001 {
                 self.stop()
-            } else if (self.player?.rate ?? 0) < 0.0001, let lastStation = self.lastStation {
-                self.play(channelIndex: self.stations.uiIndex(for: lastStation))
+            } else if (self.player?.rate ?? 0) < 0.0001, let currentStation = self.currentStation {
+                self.play(channelIndex: self.stations.uiIndex(for: currentStation))
             }
             
             return .success
